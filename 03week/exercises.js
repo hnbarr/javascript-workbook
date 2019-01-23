eedcf4a95a0d0ac950cd3c520a19665185e7b80f
Heather Barr <hnbarr@yahoo.com>
2019-01-22 18:24:53 -0600

    homework outline

---
+++ 03week/exercises.js
@@ -0,0 +1,27 @@
+// length
+
+// concat
+
+// indexOf and lastIndexOf
+
+// join
+
+// split
+
+// reverse
+
+// sort
+
+// slice
+
+// splice
+
+// push
+
+// pop
+
+// shift
+
+// unshift
+
+// forEach
