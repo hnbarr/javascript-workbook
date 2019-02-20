20678887a739cfe0c9e067b56d60ad85c917d0a4
Heather Barr <hnbarr@yahoo.com>
2019-02-20 17:23:50 -0600

    toggle between players working

--- 07week/ticTacToe/script.js
+++ 07week/ticTacToe/script.js
@@ -3,9 +3,19 @@
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
+    this.state ={
+      playerMove: 'X'
+    }
+  }
+
+  toggle = () => {
+    if(this.state.playerMove==='X'){
+      this.setState({playerMove: 'O'})
+    }else this.setState({playerMove: 'X'})
   }
 
   render() {
+    console.log('this is player change', this.state.playerMove)
     return (
       <div>
         <div className="row">
@@ -13,6 +23,7 @@ class TicTacToe extends React.Component {
           <div data-cell="1"></div>
           <div data-cell="2"></div>
         </div>
+        <button onClick={this.toggle}>test</button>
         <div className="row">
           <div data-cell="3"></div>
           <div data-cell="4"></div>
