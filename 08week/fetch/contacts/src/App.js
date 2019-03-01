c848d89f20eb2ce246810b2dab2662dc9a15da6e
Heather Barr <hnbarr@yahoo.com>
2019-02-28 22:52:13 -0600

    made app now time to create

---
+++ 08week/fetch/contacts/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Alert } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div className="jumbotron">
+          <h1>Test Test</h1>
+          <hr />
+          <p>test stuff here.  test test</p>
+          <p>another test test</p>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
