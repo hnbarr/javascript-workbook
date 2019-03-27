b5833848cb3d6877dd6953214551fe2d9b08c882
Heather Barr <hnbarr@yahoo.com>
2019-03-26 20:17:09 -0500

    worked on counter found bug need to fix

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -36,34 +36,26 @@ class Board {
     // add our column numbers, for guidance
     let string = "  0 1 2 3 4 5 6 7\n";
     for (let row = 0; row < 8; row++) {
-      // we start with our row number in our array
-      const rowOfCheckers = [row];
-      // a loop within a loop
-      for (let column = 0; column < 8; column++) {
-        // if the location is "truthy" (contains a checker piece, in this case)
+      const rowOfCheckers = [row]; // we start with our row number in our array
+      for (let column = 0; column < 8; column++) { // if the location is "truthy" (contains a checker piece, in this case)
         if (this.grid[row][column]) {
-          // push the symbol of the check in that location into the array
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
-          // just push in a blank space
-          rowOfCheckers.push(' ');
+          rowOfCheckers.push(' '); // just push in a blank space
         }
       }
-      // join the rowOfCheckers array to a string, separated by a space
-      string += rowOfCheckers.join(' ');
-      // add a 'new line'
-      string += "\n";
+      string += rowOfCheckers.join(' '); // join the rowOfCheckers array to a string, separated by a space
+      string += "\n"; // add a 'new line'
     }
     console.log(string);
   }
 
-  // Your code here
   createCheckers() {
     let whitePositions = [
       [0, 1], [0, 3], [0, 5], [0, 7],
       [1, 0], [1, 2], [1, 4], [1, 6],
       [2, 1], [2, 3], [2, 5], [2, 7]
-    ] //this creates all the white spots on the borad at start and new game.
+    ] 
 
     for (let i = 0; i < 12; i++){
       let whiteRow = whitePositions[i][0];
@@ -72,12 +64,12 @@ class Board {
       this.checkers.push(whiteChecker);
       this.grid[whiteRow][whiteColumn] = whiteChecker;
     }
-    
+
     let blackPositions = [
       [5, 0], [5, 2], [5, 4], [5, 6],
       [6, 1], [6, 3], [6, 5], [6, 7],
       [7, 0], [7, 2], [7, 4], [7, 6]
-    ] //this creates the black spots on the board at start and new game.
+    ] 
 
     for(let i = 0; i < 12; i++){
       let blackRow = blackPositions[i][0];
@@ -86,10 +78,6 @@ class Board {
       this.checkers.push(blackChecker);
       this.grid[blackRow][blackColumn] = blackChecker
     }
-  }
-
-  killChecker (position) {
-   console.log("KILLED")
   }
 }
 
@@ -103,38 +91,34 @@ class Game {
   }
   moveChecker(start, end) {
     let startRow = parseInt(start.charAt(0))
-    let startColumn = parseInt(start.charAt(1))
-    // console.log('startRow:', startRow, 'startColumn:', startColumn)
-
+    let startColumn = parseInt(start.charAt(1)) // console.log('startRow:', startRow, 'startColumn:', startColumn)
     let endRow = parseInt(end.charAt(0))
-    let endColumn = parseInt(end.charAt(1))
-    // console.log('endRow:', endRow, 'endColumn:', endColumn)
-
-    this.board.grid[endRow][endColumn] = this.board.grid[startRow][startColumn]; //moves the checker to end
-    this.board.grid[startRow][startColumn] = null; //takes away  from first  spot, bc it was moved to end
+    let endColumn = parseInt(end.charAt(1)) // console.log('endRow:', endRow, 'endColumn:', endColumn)
     
-    // jumping an enemy checker.
-    if(Math.abs(endRow - startRow) === 2){
-      this.board.killChecker();
+    this.board.grid[endRow][endColumn] = this.board.grid[startRow][startColumn]; //moves the checker & takes away from first spot
+    this.board.grid[startRow][startColumn] = null;
+    
+    if(Math.abs(endRow - startRow) === 2){ // if making a jump...
+      console.log("I took away one of your chips!")
       let midpointRow = ((startRow + endRow)/2)
       let midpointColumn = ((startColumn + endColumn)/2)
-    // to keep track of jumped chips
-      let jumpedWhite = 0 
+      let jumpedWhite = 0
       let jumpedBlack = 0
-      
+    
       if(this.board.grid[midpointRow][midpointColumn] !== null){
         if(this.board.grid[midpointRow][midpointColumn].symbol === String.fromCharCode(0x125CB)){
-          jumpedWhite ++
+          jumpedWhite++ //only adding on first jump, need to fix!
           console.log(`jumped white chips = ${jumpedWhite}`)
           this.board.grid[midpointRow][midpointColumn] = null //takes away jumped checker
           this.board.checkers.length-- //takes away one value from length.
         } else if (this.board.grid[midpointRow][midpointColumn].symbol === String.fromCharCode(0x125CF)){
-          jumpedBlack ++
+          jumpedBlack++ //only adding on first jump, need to fix!
           console.log(`jumped black chips = ${jumpedBlack}`)
           this.board.grid[midpointRow][midpointColumn] = null //takes away jumped checker
           this.board.checkers.length-- //takes away one value from length.
         } 
         console.log(`total checkers left; ${this.board.checkers.length}`)
+        return false
       }
     }
     // 50-41, 23-32, 41-23 (to test a move)
