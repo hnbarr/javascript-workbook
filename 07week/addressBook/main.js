535e5d16e9acaaf0919910d1b7fa7e5ab5e57c85
Heather Barr <hnbarr@yahoo.com>
2019-04-02 20:25:30 -0500

    almost done working on style now

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 let nextBtn = document.getElementById('next').addEventListener('click', getContact)
+let display = document.getElementById('display')
 
 function getContact(){
-    fetch('https://randomuser.me/api/')
-    .then((res)=> res.json)
+    fetch('https://randomuser.me/api/?results=5')
+    .then((res)=> res.json())
     .then((data)=> {
-        console.log(data)
+        console.log(data.results)
+        let output = '<h2> Hello, Friends </h2>'
+        data.results.forEach((contact)=> {
+            output += `
+            <ul>
+                <img src='${contact.picture.thumbnail}'> 
+                <li> name: ${contact.name.first} ${contact.name.last} </li>
+                <li> city: ${contact.location.city} </li>
+                <li> phone number: ${contact.phone} </li>
+            </ul>
+            `
+        })
+        display.innerHTML = output;
     })
 }
