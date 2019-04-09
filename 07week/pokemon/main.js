d424d9f5da7ec8f728b19aa0563f552527fbcf3e
Heather Barr <hnbarr@yahoo.com>
2019-04-08 23:33:02 -0500

    worked on extra features

--- 07week/pokemon/main.js
+++ 07week/pokemon/main.js
@@ -1,7 +1,11 @@
 "use strict";
 
 let p1 = null;
+let p1name = null;
+let p1value = null;
 let p2 = null;
+let p2name = null;
+let p2value = null;
 
 let getPokemon1 = () => {
   fetch('https://pokeapi.co/api/v2/pokemon')
@@ -10,7 +14,7 @@ let getPokemon1 = () => {
     })
     .then(function (data) {
       let random = Math.floor(Math.random() * 20)
-      console.log('randon: ', random)
+      console.log('random: ', random)
       let player = data.results[random]
       p1 = player
       return fetch(player.url) //access that api within that pokemon
@@ -20,11 +24,10 @@ let getPokemon1 = () => {
     })
     .then(function (data) {
       // getting pokemon name here!
-      let name = data.species.name
-      let baseExp1 = data.base_experience
+      p1name = data.species.name
+      p1value = data.base_experience
       console.log('name: ', name, 'p1: ', p1, 'p2: ', p2)
-      power(baseExp1)
-      
+      checkForWin()
     })
     .catch(function (error) {
       console.log('Requestfailed', error)
@@ -38,7 +41,7 @@ let getPokemon2 = () => {
       })
       .then(function (data) {
         let random = Math.floor(Math.random() * 20)
-        console.log('randon: ', random)
+        console.log('random: ', random)
         let player = data.results[random]
         p2 = player
         return fetch(player.url) //access that api within that pokemon
@@ -48,29 +51,30 @@ let getPokemon2 = () => {
       })
       .then(function (data) {
         // getting pokemon name here!
-        let name = data.species.name
-        let baseExp2 = data.base_experience
+        p2name = data.species.name
+        p2value = data.base_experience
         console.log('name: ', name, 'p1: ', p1, 'p2: ', p2)
-        power(baseExp2)
+        checkForWin()
       })
       .catch(function (error) {
         console.log('Requestfailed', error)
       });
   }
 
-  let power = (exp) => {
-    console.log(exp)
-    checkForWin()
-  }
-
   let checkForWin = ()=> {
       if(p1 !== null && p2 !== null){
-        console.log(p1.url, p2.url)
+        if(p1value === p2value){
+            console.log(`It's a draw!`)
+          } else if(p2 > p1){
+            console.log(`Winner: ${p2.name}!`)
+          } else if(p1 > p2){
+            console.log(`Winner: ${p1.name}!`)
+          }
       }
   }
 
   let restart = () => {
-      //code here
+    window.location.href=window.location.href
   }
 
 
