88f51366c2c2aecaa6d0399b3bdfab0e23c8870b
Heather Barr <hnbarr@yahoo.com>
2019-04-16 22:54:22 -0500

    working on delete

--- 09week/task-app/src/App.js
+++ 09week/task-app/src/App.js
@@ -23,10 +23,20 @@ class App extends Component {
     })
   }
 
+  deleteTask=(index)=>{
+    console.log('working here', this.state.tasks)
+    this.setState({
+      tasks: [...this.state.tasks.filter(task => task[index] !== index)]
+      // tasks: [...this.state.tasks.remove(this.state.tasks[index])]
+      // need help here, with logic. to remove clicked p
+    })
+  }
+
   handleList=()=>{
     return this.state.tasks.map((task, index)=>{
       return (
       <p key={index}>
+        <button className='delete' onClick={this.deleteTask}>Done!</button>
         {task}
       </p>
       )
@@ -37,11 +47,15 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <h1>Tasky</h1>
+          <h1>tasky</h1>
           <h6>lets do some things!</h6>
         </header>
-        <input type='text' onChange={this.handleInput} value={this.state.inputVal}></input>
-        <button onClick={this.handleClick}>Add Task</button>
+        <hr />
+        <section>
+          <input type='text' onChange={this.handleInput} value={this.state.inputVal}></input>
+          <button className='add' onClick={this.handleClick}>Add Task</button>
+        </section>
+        <hr/>
         {this.handleList()}
       </div>
     );
